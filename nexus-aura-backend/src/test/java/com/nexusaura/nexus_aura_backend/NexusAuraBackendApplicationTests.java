50fa5ca78522115a64070ab9418083f8367b7982
Leo <61775738+Leobej@users.noreply.github.com>
2024-12-28 03:14:31 +0200

    feat(backend): add initial backend project

---
+++ nexus-aura-backend/src/test/java/com/nexusaura/nexus_aura_backend/NexusAuraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusaura.nexus_aura_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusAuraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
